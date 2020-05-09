37ede14631368febb7bd348f62009163492b3fc9
Den Delimarsky <1389609+dend@users.noreply.github.com>
2020-05-09 12:56:37 -0700

    Better image capture.

--- interactive/util.js
+++ interactive/util.js
@@ -14,8 +14,32 @@ function processInput(context) {
 
 
 function capture() {
+    // Prepare video background because we know that html2canvas doesn't deal with those.
+    video = document.getElementById('video');
+    canvas = document.getElementById('staticpicture');
+    photo = document.getElementById('camera-placeholder');
+
+    var context = canvas.getContext('2d');
+
+    width = 500;
+    height = video.videoHeight / (video.videoWidth / width);
+
+    if (isNaN(height)) {
+        height = width / (4 / 3);
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+    context.drawImage(video, 0, 0, width, height);
+
+    document.querySelector('.hidden-custom-image').style.visibility = "visible"
+
+    var data = canvas.toDataURL('image/png');
+    photo.setAttribute('src', data);
+
     html2canvas(document.querySelector("#main-block")).then(canvas => {
         document.body.appendChild(canvas)
     })
-}
 
+    document.querySelector('.hidden-custom-image').style.visibility = "hidden"
+}
