9060d1d82b79869ede4875a7e36b6541beb13459
Den Delimarsky <1389609+dend@users.noreply.github.com>
2020-05-10 10:33:36 -0700

    Start integration work

--- attendee/attendees.js
+++ attendee/attendees.js
@@ -9,13 +9,13 @@ class Attendee {
 
 //add more attendees here
 let galleryAttendees = [
-  new Attendee("Clint Rutkas", "/attendee/clint.jpg"),
-  new Attendee("Den Delimarschi", "/attendee/den.jpg"),
-  new Attendee("Elon Musk", "/attendee/elon-musk.jpg"),
-  new Attendee("Golnaz", "/attendee/golnaz.jpg"),
-  new Attendee("Royal Party", "/attendee/royals.jpg"),
-  new Attendee("Tracy Morgan", "/attendee/tracy-morgan.jpg"),
-  new Attendee("Golnaz", "/attendee/golnaz.jpg"),
-  new Attendee("Royal Party", "/attendee/royals.jpg"),
-  new Attendee("Tracy Morgan", "/attendee/tracy-morgan.jpg"),
+  new Attendee("Clint Rutkas", "../attendee/clint.jpg"),
+  new Attendee("Den Delimarschi", "../attendee/den.jpg"),
+  new Attendee("Elon Musk", "../attendee/elon-musk.jpg"),
+  new Attendee("Golnaz", "../attendee/golnaz.jpg"),
+  new Attendee("Royal Party", "../attendee/royals.jpg"),
+  new Attendee("Tracy Morgan", "../attendee/tracy-morgan.jpg"),
+  new Attendee("Golnaz", "../attendee/golnaz.jpg"),
+  new Attendee("Royal Party", "../attendee/royals.jpg"),
+  new Attendee("Tracy Morgan", "../attendee/tracy-morgan.jpg"),
 ];
---
+++ src/static/js/customattendee.js
@@ -0,0 +1,63 @@
+//load events
+window.addEventListener("load", loadMeetingAttendees, false);
+window.addEventListener("load", loadGalleryCards, false);
+
+var imageToUpdate = "";
+
+function loadMeetingAttendees(attendeeCount) {
+  //assuming no querystring params to rehydrate
+
+  const meetingGrid = document.getElementById("meetingGrid");
+
+  meetingGrid.innerHTML = "";
+
+  //TODO: Change to be dynamic
+  for (let i = 0; i < 6; i++) {
+    let attendeeHtml = `<div class="item">
+            <img
+              onclick="javascript:showModal(this);"
+              class="item-image"
+              src="${galleryAttendees[i].path}"
+            />
+          </div>`;
+    meetingGrid.insertAdjacentHTML("beforeend", attendeeHtml);
+  }
+}
+/* 
+Modal Attendee Gallery
+*/
+const modal = document.getElementById("updateScreen");
+
+function showModal(imgToUpdate) {
+  modal.style.display = "block";
+  imageToUpdate = imgToUpdate;
+}
+
+function hideModal() {
+  modal.style.display = "none";
+}
+
+function updateAttendee(newimg) {
+  console.log(newimg.src);
+  imageToUpdate.src = newimg.src;
+  hideModal();
+}
+
+function loadGalleryCards() {
+  const gallery = document.getElementById("gallery");
+  galleryAttendees.forEach((a) => {
+    let name = a.name;
+    let path = a.path;
+
+    let galleryHtml = `<div class="card">
+        <img
+            onclick="javascript:updateAttendee(this);"
+            src="${a.path}" />
+        <div class="card_text">
+            ${a.name}
+        </div>
+    </div>`;
+
+    gallery.insertAdjacentHTML("beforeend", galleryHtml);
+  });
+}
