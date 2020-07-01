b267ac8918183e7f69f04ae00438530c0d32d018
Den Delimarsky <1389609+dend@users.noreply.github.com>
2020-07-01 11:33:08 -0700

    Update images

--- src/static/js/attendee.js
+++ src/static/js/attendee.js
@@ -8,7 +8,6 @@ class Attendee {
 //add more attendees here
 let galleryAttendees = [
   new Attendee("Clint Rutkas", "../static/images/attendee-clint.jpg"),
-  new Attendee("Den Delimarschi", "../static/images/attendee-den.jpg"),
   new Attendee("Golnaz", "../static/images/attendee-golnaz.jpg"),
   new Attendee("Royal Party", "../static/images/attendee-royal-family.jpg"),
   new Attendee("Tracy Morgan", "../static/images/attendee-tracy-morgan.jpg"),
